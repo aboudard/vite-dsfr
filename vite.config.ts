71e4462f8a1d269eb6d9b346e6080ead7ff857c3
aboudard <coco.boudard@gmail.com>
2025-08-28 19:26:39 +0200

    chore: configure Vite base for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/vite-dsfr/',
+  base: "/vite-dsfr/",
   plugins: [react()],
 });
